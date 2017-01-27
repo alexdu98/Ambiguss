a2809a561e701d7b6d3a98e43f643c8bbae1f79f
Alexandre <alexdu98@gmx.fr>
2017-01-27 17:10:13 +0100

    Mise à jour de la base du projet.  - Ajout des fichiers principaux pour les styles (main.css) et scripts (main.js)  - Squelette du site (dans app/Resources/views/base.html.twig)  - Renommage du controleur par défaut (default -> main)

--- src/TERM1/PlatformBundle/Controller/MainController.php
+++ src/TERM1/PlatformBundle/Controller/MainController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class DefaultController extends Controller
+class MainController extends Controller
 {
     public function indexAction()
     {

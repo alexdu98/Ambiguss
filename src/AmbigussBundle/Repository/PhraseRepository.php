b26a8ed2832f389181a0c022b49b7054b75f4712
Alexandre <alexdu98@gmx.fr>
2017-04-09 00:03:40 +0200

    Rajout gain dans classement phrase

--- src/AmbigussBundle/Repository/PhraseRepository.php
+++ src/AmbigussBundle/Repository/PhraseRepository.php
@@ -11,8 +11,10 @@ namespace AmbigussBundle\Repository;
 class PhraseRepository extends \Doctrine\ORM\EntityRepository
 {
     public function getClassementPhrases($limit){
-	    return $this->createQueryBuilder('p')->select("p.contenu, p.dateCreation ")
-		    ->leftJoin("p.likesPhrase", "lp", 'with', 'lp.phrase = p.id AND lp.active = 1')->addSelect('count(lp) as nbLikes')
+	    return $this->createQueryBuilder('p')->select("p.contenu, p.dateCreation ")->distinct()
+		    ->addSelect('(SELECT COUNT(lp2.id) FROM AmbigussBundle\Entity\AimerPhrase lp2 WHERE lp2.phrase = p.id AND lp2.active = 1) as nbLikes')
+		    ->leftJoin("p.likesPhrase", "lp", 'WITH', 'lp.id = p.id')
+		    ->leftJoin("p.parties", "pa", 'WITH', 'pa.phrase = p.id')->addSelect('sum(pa.gainCreateur) as nbPoints')
             ->groupBy('p.id')
             ->orderBy('nbLikes', 'DESC')
             ->setMaxResults($limit)

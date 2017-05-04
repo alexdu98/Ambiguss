0f49f58efa83a02dc85d16fbf8ff721e1062065c
Alexandre <alexdu98@gmx.fr>
2017-05-04 23:51:43 +0200

    Affichage du menu pour les visiteurs

--- src/AmbigussBundle/Controller/ClassementController.php
+++ src/AmbigussBundle/Controller/ClassementController.php
@@ -10,32 +10,68 @@ namespace AmbigussBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ClassementController extends Controller{
+class ClassementController extends Controller
+{
 
-	public function classementGeneralAction(){
-		$repository = $this->getDoctrine()->getManager()->getRepository('UserBundle:Membre');
-		$classement = $repository->getClassementGeneral($this->getParameter('maxResultForClassementGeneral'));
+	public function classementGeneralAction()
+	{
+		if($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+		{
+			$repository = $this->getDoctrine()->getManager()->getRepository('UserBundle:Membre');
+			$classement = $repository->getClassementGeneral($this->getParameter('maxResultForClassementGeneral'));
 
-		return $this->render('AmbigussBundle:Classement:points.html.twig', array (
-			'classement' => $classement,
-		));
+			return $this->render('AmbigussBundle:Classement:points.html.twig', array(
+				'classement' => $classement,
+			));
+		}
+		else
+		{
+
+			$this->get('session')->getFlashBag()->add('erreur', "Vous devez être connecté.");
+
+			return $this->redirectToRoute('user_connexion');
+		}
 	}
-    public function classementPersonnelAction(){
-        $repository = $this->getDoctrine()->getManager()->getRepository('AmbigussBundle:Phrase');
-	    $classement = $repository->getClassementPhrasesUser($this->getUser());
 
-        return $this->render('AmbigussBundle:Classement:phrasesUser.html.twig', array (
-	        'classement' => $classement,
-	        'dureeAvantJouabiliteSecondes' => $this->getParameter('dureeAvantJouabiliteSecondes'),
-        ));
-    }
-    public function classementPhrasesAction(){
-        $repository = $this->getDoctrine()->getManager()->getRepository('AmbigussBundle:Phrase');
-        $classement = $repository->getClassementPhrases($this->getParameter('maxResultForClassementPhrases'));
+	public function classementPersonnelAction()
+	{
+		if($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+		{
+			$repository = $this->getDoctrine()->getManager()->getRepository('AmbigussBundle:Phrase');
+			$classement = $repository->getClassementPhrasesUser($this->getUser());
 
-        return $this->render('AmbigussBundle:Classement:phrases.html.twig', array (
-            'classement' => $classement,
-        ));
-    }
+			return $this->render('AmbigussBundle:Classement:phrasesUser.html.twig', array(
+				'classement' => $classement,
+				'dureeAvantJouabiliteSecondes' => $this->getParameter('dureeAvantJouabiliteSecondes'),
+			));
+		}
+		else
+		{
+
+			$this->get('session')->getFlashBag()->add('erreur', "Vous devez être connecté.");
+
+			return $this->redirectToRoute('user_connexion');
+		}
+	}
+
+	public function classementPhrasesAction()
+	{
+		if($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+		{
+			$repository = $this->getDoctrine()->getManager()->getRepository('AmbigussBundle:Phrase');
+			$classement = $repository->getClassementPhrases($this->getParameter('maxResultForClassementPhrases'));
+
+			return $this->render('AmbigussBundle:Classement:phrases.html.twig', array(
+				'classement' => $classement,
+			));
+		}
+		else
+		{
+
+			$this->get('session')->getFlashBag()->add('erreur', "Vous devez être connecté.");
+
+			return $this->redirectToRoute('user_connexion');
+		}
+	}
 
 }

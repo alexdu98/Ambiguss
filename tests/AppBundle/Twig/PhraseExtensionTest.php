cbd18c9487bde73abe3412e0ab8b74a306f2cc90
Alex <alexdu98@gmx.fr>
2019-04-22 13:19:52 +0200

    Add tests/Twig

---
+++ tests/AppBundle/Twig/PhraseExtensionTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\AppBundle\Twig;
+
+use AppBundle\Twig\PhraseExtension;
+use AppBundle\Entity\Phrase;
+use PHPUnit\Framework\TestCase;
+
+class PhraseExtensionTest extends TestCase
+{
+    protected $contenu1 = 'Une phrase <amb id="1">avec</amb> un mot ambigu.';
+    protected $attendu1 = 'Une phrase <b id="ma1" class="ma color-red" title="Ce mot est ambigu (id : 1)">avec</b> un mot ambigu.';
+
+    protected $contenu2 = 'Une phrase <amb id="1">avec</amb> un <amb id="2">mot</amb> ambigu.';
+    protected $attendu2 = 'Une phrase <b id="ma1" class="ma color-red" title="Ce mot est ambigu (id : 1)">avec</b> un <b id="ma2" class="ma color-red" title="Ce mot est ambigu (id : 2)">mot</b> ambigu.';
+
+    public function testGetStaticHTMLObject()
+    {
+        $extension = new PhraseExtension();
+
+        $phrase = new Phrase();
+
+        $phrase->setContenu($this->contenu1);
+        $res = $extension->getStaticHTML($phrase);
+        $this->assertEquals($this->attendu1, $res);
+
+        $phrase->setContenu($this->contenu2);
+        $res = $extension->getStaticHTML($phrase);
+        $this->assertEquals($this->attendu2, $res);
+    }
+
+    public function testGetStaticHTMLString()
+    {
+        $extension = new PhraseExtension();
+
+        $res = $extension->getStaticHTML($this->contenu1);
+        $this->assertEquals($this->attendu1, $res);
+
+        $res = $extension->getStaticHTML($this->contenu2);
+        $this->assertEquals($this->attendu2, $res);
+    }
+}

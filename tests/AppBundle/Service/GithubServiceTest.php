38a317857561b359d620f5964be41ac410069362
Alex <alexdu98@gmx.fr>
2019-05-01 22:16:32 +0200

    Add tests/Service/Github

---
+++ tests/AppBundle/Service/GithubServiceTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\AppBundle\Service;
+
+use \stdClass;
+use AppBundle\Service\GithubService;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class GithubServiceTest extends KernelTestCase
+{
+    private $container;
+    private $service;
+
+    protected function setUp()
+    {
+        $kernel = self::bootKernel();
+
+        $this->container = $kernel->getContainer();
+        $this->service = $this->container->get(GithubService::class);
+    }
+
+    public function testGetLastDev()
+    {
+        $game = $this->service->getLastDev();
+
+        $this->assertInstanceOf(stdClass::class, $game);
+    }
+
+    public function testGetActualCommit()
+    {
+        $game = $this->service->getActualCommit();
+
+        $this->assertInstanceOf(stdClass::class, $game);
+    }
+
+}

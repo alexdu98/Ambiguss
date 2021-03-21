67027de539b20288fab861d33717562cd91c97b5
Alexdu98 <alexdu98@gmx.fr>
2021-03-21 18:26:54 +0100

    Add headers

--- web/app.php
+++ web/app.php
@@ -17,5 +17,26 @@ if (PHP_VERSION_ID < 70000) {
 //Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
+
+$response->headers->addCacheControlDirective('private', true);
+$response->headers->addCacheControlDirective('no-cache', true);
+$response->headers->addCacheControlDirective('no-store', true);
+$response->headers->addCacheControlDirective('must-revalidate', true);
+$response->headers->add(array(
+    'X-Frame-Options' => 'deny',
+    'X-Content-Type-Options' => 'nosniff',
+    'Strict-Transport-Security' => 'max-age: 15552000',
+    'Content-Security-Policy' => "" .
+        "default-src 'self' ambiguss.fr ambiguss.test;" .
+        "script-src 'self' 'unsafe-inline' *.google.com *.gstatic.com *.facebook.net *.twitter.com;" .
+        "style-src 'self' 'unsafe-inline' *.googleapis.com;" .
+        "img-src 'self' *.twitter.com;" .
+        "font-src 'self' *.googleapis.com *.gstatic.com;" .
+        "frame-src *.google.com *.facebook.com *.twitter.com;" .
+        "block-all-mixed-content;" .
+        "upgrade-insecure-requests;",
+    'Pragma' => 'no-cache'
+));
+
 $response->send();
 $kernel->terminate($request, $response);
